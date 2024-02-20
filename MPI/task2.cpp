9ab25fe2d94b6b27a36fb7052c44b10a3688d75c
Ilya Savchenko <97258310+st1gmat@users.noreply.github.com>
2024-02-20 20:59:47 +0300

    added mpi task2

---
+++ MPI/task2.cpp
@@ -0,0 +1,47 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+//#include <iomanip>
+
+//Посчитать сумму массива чисел. Показать зависимость времени работы программы от
+//количества чисел в массиве(10, 1000, 10000000).
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    const int num_sizes = 3;
+    int sizes[num_sizes] = { 10, 1000, 10000000 };
+
+    for (int s = 0; s < num_sizes; ++s) {
+        std::vector<int> array(sizes[s]);
+        long long int sum = 0;
+
+        
+        srand(time(nullptr) + rank); // + rank чтобы при новой генерации рандомные числа не повторялись
+        for (int i = 0; i < sizes[s]; ++i) {
+            array[i] = rand() % 100;
+        }
+
+        // подсчет суммы и замер времени
+        double start = MPI_Wtime();
+        long long int local_sum = 0;
+        for (int i = 0; i < sizes[s]; ++i) {
+            local_sum += array[i];
+        }
+        double end = MPI_Wtime();
+
+        std::cout << "process " << rank << ": array size = " << sizes[s] << ", time= " << (end - start) << std::endl;
+        /*if (rank == 0) {
+            double time = end - start;
+            std::cout << std::setw(15) << sizes[s] << std::setw(15) << time << std::endl;
+        }*/
+    }
+
+    MPI_Finalize();
+    return 0;
+}
